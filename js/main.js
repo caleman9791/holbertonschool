ca742665696dab84a652740fe8b04a7355cedf98
caleman9791 <caleman9791@protonmail.com>
2021-10-24 00:09:40 -0400

    ok

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let btn_icon = document.querySelector(".btn_icon");
+
+btn_icon.addEventListener("click", function() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+});

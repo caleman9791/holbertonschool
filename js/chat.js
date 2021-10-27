eafbd33bcaf96612dc6628b7742b046c96203dfe
caleman9791 <caleman9791@protonmail.com>
2021-10-27 08:34:55 -0400

    ok

---
+++ js/chat.js
@@ -0,0 +1,28 @@
+ // var _smartsupp = _smartsupp || {};
+ // _smartsupp.key = '55edc32d6709e188be4f139b09cbdf5d9065e43a';
+ // window.smartsupp || (function(d) {
+ //   var s, c, o = smartsupp = function() {
+ //     o._.push(arguments)
+ //   };
+ //   o._ = [];
+ //   s = d.getElementsByTagName('script')[0];
+ //   c = d.createElement('script');
+ //   c.type = 'text/javascript';
+ //   c.charset = 'utf-8';
+ //   c.async = true;
+ //   c.src = 'https://www.smartsuppchat.com/loader.js?';
+ //   s.parentNode.insertBefore(c, s);
+ // })(document);
+
+
+ var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+ (function() {
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = 'https://embed.tawk.to/61794403f7c0440a59203d23/1fj0r3cce';
+  s1.charset = 'UTF-8';
+  s1.setAttribute('crossorigin', '*');
+  s0.parentNode.insertBefore(s1, s0);
+ })();

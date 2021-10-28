86732e4b268722cf601e74ff3a19d5d7560b7e25
caleman9791 <caleman9791@protonmail.com>
2021-10-27 23:47:52 -0400

    ok

--- js/flexbox.js
+++ js/flexbox.js
@@ -1,1 +1,48 @@
-flexbox.js
+class Item {
+  constructor() {
+    this.element = document.createElement("div");
+    return this.element;
+  }
+
+}
+
+
+let generator = document.querySelector(".generator");
+generator.style.display = "flex";
+
+let element_quantity = document.querySelector(".element_quantity");
+let element = [];
+// removeAllChildNodes(generator);
+
+element_quantity.addEventListener("change", function(argument) {
+
+  generateNodes(this.value);
+
+});
+
+element_quantity.addEventListener("keyup", function(argument) {
+
+  generateNodes(this.value);
+
+});
+
+
+function generateNodes(count) {
+  removeAllChildNodes(generator);
+  element = [];
+  for (var i = 0; i < count; i++) {
+    element[i] = new Item();
+    element[i].textContent = "xx";
+    element[i].style.border = "1px solid black";
+  }
+
+  for (var i = 0; i < element.length; i++) {
+    generator.appendChild(element[i]);
+  }
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}

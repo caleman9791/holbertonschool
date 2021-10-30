b4e077e33755aeb8622291f2c6e12fa34c28d79c
caleman9791 <caleman9791@protonmail.com>
2021-10-30 07:46:47 -0400

    ok

--- js/flexbox.js
+++ js/flexbox.js
@@ -1,54 +1,54 @@
-class Item {
-  constructor() {
-    this.element = document.createElement("div");
-    return this.element;
-  }
+// class Item {
+//   constructor() {
+//     this.element = document.createElement("div");
+//     return this.element;
+//   }
 
-}
-
-
-let generator = document.querySelector(".generator");
-generator.style.display = "flex";
-
-let element_quantity = document.querySelector(".element_quantity");
-let element = [];
-// removeAllChildNodes(generator);
-
-element_quantity.addEventListener("change", function(argument) {
-
-  generateNodes(this.value);
-
-});
-
-element_quantity.addEventListener("keyup", function(argument) {
-
-  generateNodes(this.value);
-
-});
+// }
 
 
-function generateNodes(count) {
-  removeAllChildNodes(generator);
-  element = [];
-  if (count > 12) {
-    count = 12;
-  }
-  for (var i = 0; i < count; i++) {
-    element[i] = new Item();
-    element[i].textContent = "xx";
-    element[i].style.border = "1px solid black";
-  }
+// let generator = document.querySelector(".generator");
+// generator.style.display = "flex";
 
-  for (var i = 0; i < element.length; i++) {
-    generator.appendChild(element[i]);
-  }
-}
+// let element_quantity = document.querySelector(".element_quantity");
+// let element = [];
+// // removeAllChildNodes(generator);
 
-function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
-}
+// element_quantity.addEventListener("change", function(argument) {
+
+//   generateNodes(this.value);
+
+// });
+
+// element_quantity.addEventListener("keyup", function(argument) {
+
+//   generateNodes(this.value);
+
+// });
+
+
+// function generateNodes(count) {
+//   removeAllChildNodes(generator);
+//   element = [];
+//   if (count > 12) {
+//     count = 12;
+//   }
+//   for (var i = 0; i < count; i++) {
+//     element[i] = new Item();
+//     element[i].textContent = "xx";
+//     element[i].style.border = "1px solid black";
+//   }
+
+//   for (var i = 0; i < element.length; i++) {
+//     generator.appendChild(element[i]);
+//   }
+// }
+
+// function removeAllChildNodes(parent) {
+//   while (parent.firstChild) {
+//     parent.removeChild(parent.firstChild);
+//   }
+// }
 
 // window.onresize = function(event) {
 //   let box_align_content = document.querySelector(".box_align_content .x");

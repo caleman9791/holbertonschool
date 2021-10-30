39286a3bce59d093e7ca95f27631a0ceda16ad9b
caleman9791 <caleman9791@protonmail.com>
2021-10-29 21:16:41 -0400

    ok

--- js/tweets.js
+++ js/tweets.js
@@ -1,1 +1,23 @@
-tweets.js
+document.addEventListener('readystatechange', event => {
+
+
+  let seconds = 10;
+  let intervalID = setInterval(function(argument) {
+    seconds -= 1;
+
+    if (seconds == 0) {
+      let tweets_container = document.querySelector(".tweets_container");
+
+      tweets_container.style.display = "block";
+      clearInterval(intervalID);
+
+    }
+
+    if (seconds == 1) {
+      let charging = document.querySelector(".charging");
+      charging.style.display = "none";
+    }
+  }, 1000, );
+
+
+});
